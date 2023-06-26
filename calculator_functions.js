ba473bf2ec146f37f6ffb2cdb6a7832410391bf4
Martysim <martin.simeonov@scalefocus.com>
2023-06-26 14:24:45 +0300

    Create new functionality in progress

--- calculator_functions.js
+++ calculator_functions.js
@@ -1,12 +1,100 @@
-let arrOfNumAnsOper = [];
 
-function number(value) {
-    if (['+', '-', '*', '/', '='].includes(value)) {
+const clearButton = document.getElementById('clearButton');
+const negateButton = document.getElementById('negateButton');
+const percentageButton = document.getElementById('percentageButton');
+const divideButton = document.getElementById('divideButton');
+
+const sevenButton = document.getElementById('sevenButton');
+const eightButton = document.getElementById('eightButton');
+const nineButton = document.getElementById('nineButton');
+const multiplyButton = document.getElementById('multiplyButton');
+
+const fourButton = document.getElementById('fourButton');
+const fiveButton = document.getElementById('fiveButton');
+const sixButton = document.getElementById('sixButton');
+const subtractButton = document.getElementById('subtractButton');
+
+const oneButton = document.getElementById('oneButton');
+const twoButton = document.getElementById('twoButton');
+const threeButton = document.getElementById('threeButton');
+const addButton = document.getElementById('addButton');
+
+const zeroButton = document.getElementById('zeroButton');
+const decimalButton = document.getElementById('decimalButton');
+const equalsButton = document.getElementById('equalsButton');
+
+
+clearButton.addEventListener('click', handleClear);
+negateButton.addEventListener('click', handleNegate);
+percentageButton.addEventListener('click', handlePercentage);
+divideButton.addEventListener('click', handleOperator);
+
+sevenButton.addEventListener('click', handleNumberClick);
+eightButton.addEventListener('click', handleNumberClick);
+nineButton.addEventListener('click', handleNumberClick);
+multiplyButton.addEventListener('click', handleOperatorClick);
+
+fourButton.addEventListener('click', handleNumberClick);
+fiveButton.addEventListener('click', handleNumberClick);
+sixButton.addEventListener('click', handleNumberClick);
+subtractButton.addEventListener('click', handleOperatorClick);
+
+oneButton.addEventListener('click', handleNumberClick);
+twoButton.addEventListener('click', handleNumberClick);
+threeButton.addEventListener('click', handleNumberClick);
+addButton.addEventListener('click', handleOperatorClick);
+
+zeroButton.addEventListener('click', handleNumberClick);
+decimalButton.addEventListener('click', handleNumberClick);
+equalsButton.addEventListener('click', handleNumberClick);
+
+function handleClear() {
+    arrOfNumAnsOper = [];
+    updateResult('');
+};
+
+function handleNegate() {
+    if (arrOfNumAnsOper.length > 0) {
+        const currentNumber = arrOfNumAnsOper[arrOfNumAnsOper.length - 1];
+        arrOfNumAnsOper[arrOfNumAnsOper.length - 1] = -currentNumber;
+        updateResult(arrOfNumAnsOper[arrOfNumAnsOper.length - 1]);
+    };
+};
+
+function handlePercentage() {
+    if (arrOfNumAnsOper.length > 0) {
+        const currentNumber = arrOfNumAnsOper[arrOfNumAnsOper.length - 1];
+        const percentage = currentNumber / 100;
+        arrOfNumAnsOper[arrOfNumAnsOper.length - 1] = percentage;
+        updateResult(percentage);
+    };
+};
+
+function handleOperator() {
+    if (arrOfNumAnsOper.length >= 3) {
         performOperation();
-    } else {
-        arrOfNumAnsOper.push(Number(value));
-    }
-}
+    };
+
+    const operator = event.target.textContent;
+    if (arrOfNumAnsOper.length === 1) {
+        arrOfNumAnsOper.push(operator);
+    };
+};
+
+function handleNumberClick(event) {
+    const number = event.target.textContent;
+    arrOfNumAnsOper.push(Number(number));
+    updateResult(number);
+};
+
+function handleOperatorClick(event) {
+    if (arrOfNumAnsOper.length >= 3) {
+        performOperation();
+    };
+
+    const operator = event.target.textContent;
+    arrOfNumAnsOper.push(operator);
+};
 
 function performOperation() {
     const operand1 = arrOfNumAnsOper[0];
@@ -30,14 +118,19 @@ function performOperation() {
         default:
             result = 'Error';
             break;
-    }
+    };
 
     arrOfNumAnsOper = [result];
     updateResult(result);
-}
+};
 
 function updateResult(result) {
     const resultElement = document.querySelector('.result');
     resultElement.textContent = result;
-}
+};
+
+
+
+
+
 
